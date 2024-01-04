8c0b896c3800097575c42b2849975b69520ab1ee
heeneth <heeneth123@gmail.com>
2024-01-05 00:06:47 +0530

    added contact

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Contact() {
+  return (
+    <>
+      <div>
+        <h1>Contact</h1>
+      </div>
+    </>
+  );
+}
+
+export default Contact;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import NavBar from "../components/NavBar";
 import Body from "../components/Body";
+import Contact from "../components/Contact";
 
 function Home() {
   return (
     <>
       <NavBar />
       <Body />
+      <Contact />
     </>
   );
 }
